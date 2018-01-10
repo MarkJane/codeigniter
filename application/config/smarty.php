615e5cb45f7f8638a1855ada1ef3a2c24c60b3c4
bingoladen <smallxi5@qq.com>
2018-01-10 18:08:08 +0800

    常规更新

--- application/config/smarty.php
+++ application/config/smarty.php
@@ -1,4 +1,5 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * Project:     Smarty config: the PHP compiling template engine config
